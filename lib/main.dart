17dc6568a36207dfb6347fa67f4cd3e7a9952601
Williambz12 <williamsilva754@gmail.com>
2024-11-28 11:06:26 -0300

    video 2.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:background_services/app/services/couchbase_service.dart';
 import 'package:cbl_flutter/cbl_flutter.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +12,35 @@ import 'app/utils/couchbase_constants.dart';
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
+    final userId = inputData?['userId'];
+    print("Executando tarefa com userId: $userId");
     final service = CouchbaseService();
-    bool synced = false;
-    await service.startReplication(
-      collectionName: CouchbaseContants.collection,
-      onSynced: () {
-        synced = true;
-      },
-    );
-    return Future.value(synced);
+
+    if (task == 'couchbaseSync') {
+      bool synced = false;
+      await service.startReplication(
+        collectionName: CouchbaseContants.collection,
+        onSynced: () {
+          synced = true;
+        },
+      );
+
+      if (!synced && Platform.isIOS) {
+        Workmanager().registerOneOffTask(
+          "retryTask",
+          "retrySyncExecution",
+        );
+      }
+      return Future.value(synced);
+    } else if (task == 'retrySyncExecution') {
+      await service.startReplication(
+        collectionName: CouchbaseContants.collection,
+        onSynced: () {},
+      );
+      return Future.value(true);
+    } else {
+      return Future.value(true);
+    }
   });
 }
 
