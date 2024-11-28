efca77092f03d02c79a2dc70e30e7a38aaccba15
Williambz12 <williamsilva754@gmail.com>
2024-11-28 10:37:02 -0300

    video 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:background_services/app/services/couchbase_service.dart';
 import 'package:cbl_flutter/cbl_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:workmanager/workmanager.dart';
 
 import 'app/app_widget.dart';
+import 'app/utils/couchbase_constants.dart';
 
+@pragma('vm:entry-point')
 void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) {
-    print("Tarefa em background executada: $task");
-    return Future.value(true);
+  Workmanager().executeTask((task, inputData) async {
+    final service = CouchbaseService();
+    bool synced = false;
+    await service.startReplication(
+      collectionName: CouchbaseContants.collection,
+      onSynced: () {
+        synced = true;
+      },
+    );
+    return Future.value(synced);
   });
 }
 
