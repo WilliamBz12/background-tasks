5e63cab4ee7b9cc17f7e0a2201681c52fb564ae9
Williambz12 <williamsilva754@gmail.com>
2024-12-05 15:39:14 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:cbl_flutter/cbl_flutter.dart';
+import 'package:flutter/material.dart';
+
+import 'app/app_widget.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await CouchbaseLiteFlutter.init();
+
+  runApp(const MyApp());
+}
